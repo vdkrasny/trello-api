10240b2532b7e97c07a478d253531679c2c747ec
Uladzislau Krasny <uladzislau.krasny@itechart-group.com>
2019-06-27 19:45:11 +0300

    Rules for access permissions to resources have been implemented.

--- src/routes/boardsRoutes.js
+++ src/routes/boardsRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { boardsController } = require('../controllers');
+const { authController, boardsController } = require('../controllers');
 
 const router = express.Router();
 
+router.use(authController.isAuth);
 router.get('/', boardsController.findAll);
 router.get('/:boardId', boardsController.findById);
-router.post('/', boardsController.create);
-router.put('/:boardId', boardsController.findByIdAndUpdate);
-router.delete('/:boardId', boardsController.findByIdAndDelete);
+router.post('/', authController.isAdmin, boardsController.create);
+router.put('/:boardId', authController.isAdmin, boardsController.findByIdAndUpdate);
+router.delete('/:boardId', authController.isAdmin, boardsController.findByIdAndDelete);
 
 module.exports = router;
