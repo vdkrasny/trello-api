478f573275216780a0c1954fece65bcf227e8f34
Uladzislau Krasny <uladzislau.krasny@itechart-group.com>
2019-06-27 18:51:14 +0300

    Added new dependencies.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.listen(port, () => console.log(`App running on port ${port}!`));
