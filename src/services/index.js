a90c0ed8947c4891b529a9603768724b6ac34afc
Vlad Krasny <uladzislau.krasny@itechart-group.com>
2019-07-30 11:08:30 +0300

    Index file was added to the services folder.

---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+const AuthService = require('./AuthService');
+const BoardService = require('./BoardService');
+const CardService = require('./CardService');
+
+module.exports = {
+    AuthService,
+    BoardService,
+    CardService
+};
