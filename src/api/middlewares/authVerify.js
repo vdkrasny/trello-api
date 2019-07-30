4d8ebdcefab5cb4376f66ac9c30552b6394bed95
Vlad Krasny <uladzislau.krasny@itechart-group.com>
2019-07-30 11:06:54 +0300

    The token error in authVerify method was removed.

--- src/api/middlewares/authVerify.js
+++ src/api/middlewares/authVerify.js
@@ -6,15 +6,15 @@ const StatusError = require('../../helpers/StatusError');
 module.exports = (request, response, next) => {
     const token = request.headers[config.headers.authToken];
 
-    if (!token) return next(new StatusError(401, 'Access denied. No token provided.'));
+    if (!token) return next();
 
     try {
         const verifiedUser = jwt.verify(token, config.jwt.secret);
 
         request.user = verifiedUser;
+
+        return next();
     } catch (error) {
         return next(new StatusError(400, 'Invalid token.'));
     }
-
-    return next();
 };
