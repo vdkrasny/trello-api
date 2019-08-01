2603672b320298b161f37edea73703b5d04321ed
Vlad Krasny <uladzislau.krasny@itechart-group.com>
2019-08-01 14:32:23 +0300

    Index file was removed from the services folder

--- src/services/index.js
+++
@@ -1,9 +0,0 @@
-const AuthService = require('./AuthService');
-const BoardService = require('./BoardService');
-const CardService = require('./CardService');
-
-module.exports = {
-    AuthService,
-    BoardService,
-    CardService
-};
