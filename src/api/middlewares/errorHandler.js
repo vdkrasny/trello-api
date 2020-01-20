d284d4065a2bba17c5db67f8ea19aa8e6d92ea3b
Vlad Krasny <Vlad.Krasny@coxautoinc.com>
2020-01-20 18:47:00 +0300

    Fixed dependency of the error message on itself.

--- src/api/middlewares/errorHandler.js
+++ src/api/middlewares/errorHandler.js
@@ -2,7 +2,7 @@ import { HttpStatus } from '../../enums/HttpStatus';
 
 export default (error, request, response, next) => {
     const status = error.httpStatus || HttpStatus.InternalServerError;
-    const message = error.message || 'Something went wrong';
+    const message = error.httpStatus ? error.message : 'Something went wrong';
 
     return response.status(status).json({
         error: { message },
